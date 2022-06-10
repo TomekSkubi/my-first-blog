882a6f7690d081ff3c9c01591c7853e78c5d3eb9
TomekSkubi <skubs@o2.pl>
2022-06-10 19:21:41 +0200

    tresc

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-tgvkv0#-4g4@_@^ays^n^x64u&s-z)8wa$bj3s0q&26nv6m_cw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
